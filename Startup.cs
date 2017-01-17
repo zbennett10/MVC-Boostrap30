7e74dca2d0c5007cfe091c7eb1b211752329a0cf
Zachary Bennett <zbennett10@hotmail.com>
2017-01-17 13:17:25 -0500

    initial commit

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BootstrappingMVC.Startup))]
+namespace BootstrappingMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
